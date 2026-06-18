e0c047eea23ac6bb172fea67b992716ae909bc6d
JAL101918 <mrjacklooney0@gmail.com>
2026-06-18 21:29:22 +0700

    Combine?

--- obj/Debug/net8.0/CS_Team_Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/CS_Team_Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_Team_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb7fbcf0cbeb10cf6393833b27c98896698285f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35207c7c634021113f8dca73223fe4207f40c0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_Team_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_Team_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
